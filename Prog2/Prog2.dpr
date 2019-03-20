f3244de2d6c7f8e177d7b3adbd2c9a2c067362db
Robson Rodrigues <robson@r2xsistemas.com>
2019-03-20 14:55:35 -0300

    Nova Pasta

---
+++ Prog2/Prog2.dpr
@@ -0,0 +1,14 @@
+program Prog2;
+
+uses
+  Vcl.Forms,
+  UPrinc in 'UPrinc.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
